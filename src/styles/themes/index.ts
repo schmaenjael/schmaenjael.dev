b6566d7ae69548948c69a63ac36719b0590a0d7e
schmaenjael <manuelmayer04@gmail.com>
2022-10-10 21:55:56 +0200

    feat(styling): add reset and basic font template, as well as the theming function

---
+++ src/styles/themes/index.ts
@@ -0,0 +1,23 @@
+export const dark = {
+  background: '#292e3dff',
+  darkerBackground: '#1b1e2b',
+  lighterBackground: '#2a3040ff',
+  seperator: '#cccccc1a',
+  action: '#6b51a3ff',
+  inactiveText: '#ffffff80',
+  text: '#ffffffff',
+  titleText: '#5565e8ff',
+  commentText: '#607b96ff',
+};
+
+export const light = {
+  background: '#ffffff',
+  darkerBackground: '#1b1e2b',
+  lighterBackground: '#2a3040ff',
+  seperator: '#cccccc1a',
+  action: '#6b51a3ff',
+  inactiveText: '#ffffff80',
+  text: '#ffffffff',
+  titleText: '#5565e8ff',
+  commentText: '#607b96ff',
+};
