846267ac65a04806db6f9b59dbe98dd4ad3ed554
schmaenjael <manuelmayer04@gmail.com>
2022-10-11 18:16:06 +0200

    feat(styling): integrate eslintrc and prettier further

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,5 @@
-import { useTranslation } from 'react-i18next';
-import { useOutletContext } from '@remix-run/react';
+import { LoaderFunction, redirect } from '@remix-run/node';
+import i18next from 'src/services/locales/i18next.server';
 
-const Index = () => {
-  const { t } = useTranslation('common');
-  const { theme, setTheme } = useOutletContext<Settings>();
-
-  return (
-    <main>
-      <h1>{t('welcome')}</h1>
-      <button
-        onClick={() => {
-          if (theme === 'dark') setTheme('light');
-          else setTheme('dark');
-        }}
-      >
-        {theme}
-      </button>
-    </main>
-  );
-};
-
-export default Index;
+export const loader: LoaderFunction = async ({ request }) =>
+  redirect(`${await i18next.getLocale(request)}/`);
