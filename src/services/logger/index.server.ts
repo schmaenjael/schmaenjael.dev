f8470443674125285cf2ae99bcefa256ba03dba5
schmaenjael <manuelmayer04@gmail.com>
2022-10-23 20:08:13 +0200

    feat(404): improved documentation slightly

--- src/services/logger/index.server.ts
+++ src/services/logger/index.server.ts
@@ -5,14 +5,15 @@ import chalk from 'chalk';
  * An enum of valid logging levels.
  */
 export enum LogLevel {
-  Error,
-  Warn,
-  Default,
-  Debug,
+  Error = 0,
+  Warn = 1,
+  Default = 2,
+  Debug = 3,
 }
 
 /**
  * @description Server-Side Logger
+ * @internal
  *
  * @see [Inspiration](https://github.com/vendure-ecommerce/vendure)
  */
@@ -25,42 +26,52 @@ export class Logger {
     this.timestamp = options?.timestamp ?? true;
   }
 
-  /** @internal */
   error(message: string, trace?: object | string | undefined) {
     if (this.level <= LogLevel.Error) return;
 
     const output = trace ? `${message}: ${JSON.stringify(trace)}` : message;
-    const timestamp = new Date(Date.now()).toLocaleString(undefined);
 
-    process.stderr.write(
-      `[${chalk.red(`ERROR`)}] ${timestamp} - ${chalk.cyan(output)} \n`
-    );
+    this.printError(chalk.yellow(`ERROR`), output);
   }
 
-  /** @internal */
+  /** @description prints warnings to the standard output */
   warn(message: string) {
     if (this.level <= LogLevel.Warn) return;
     this.print(chalk.yellow(`WARNING`), message);
   }
 
-  /** @internal */
+  /** @description prints infos to the standard output */
   info(message: string) {
     if (this.level <= LogLevel.Default) return;
     this.print(chalk.blueBright(`INFO`), message);
   }
 
-  /** @internal */
+  /** @description prints debug information to the standard output */
   debug(message: string) {
     if (this.level <= LogLevel.Debug) return;
     this.print(chalk.magenta(`DEBUG`), message);
   }
 
+  /** @internal */
   private print(prefix: string, message: string) {
+    // retrieves current time
     const timestamp = new Date(Date.now()).toLocaleString(undefined);
     process.stdout.write(
       `[${prefix}] ${timestamp} - ${chalk.cyan(message)} \n`
     );
   }
+
+  /** @internal */
+  private printError(prefix: string, message: string) {
+    // retrieves current time
+    const timestamp = new Date(Date.now()).toLocaleString(undefined);
+    process.stderr.write(
+      `[${prefix}] ${timestamp} - ${chalk.cyan(message)} \n`
+    );
+  }
 }
 
-export default new Logger({ level: LogLevel.Default });
+export default new Logger({
+  level:
+    process.env.NODE_ENV === 'production' ? LogLevel.Default : LogLevel.Debug,
+});
